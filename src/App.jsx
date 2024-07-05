9e875cddc8d5dea881f499ce984c0f122fec722f
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-07-05 10:35:01 +0530

    Campus

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
 import Title from "./components/Title/Title";
 import About from "./components/About/About";
+import Campus from "./components/Campus/Campus";
 
 const App = () => {
   return (
@@ -14,6 +15,10 @@ const App = () => {
         <Title subtitle="OUR PROGRAM" title="What We Offer"/>
         <Programs />
         <About/>
+        <Title subtitle="Gallery" title="Campus Photos"/>
+        <Campus/>
+        <Title subtitle="Gallery" title="Campus Photos"/>
+
       </div>
     </>
   );
